931d51bc5b0d297e64c9fc625cc66a4f511facb2
Maksim Makaranka <maksim.makaranka@gmail.com>
2024-12-08 21:56:05 +0100

    Fix answer streaming

--- code/src/server.py
+++ code/src/server.py
@@ -47,11 +47,13 @@ async def ask(user_request, location: str):
             "symptoms_categories": [],
             "datetime": "",
         }
-        for chunk in graph.stream(initial_state):
-            for node_name, node_results in chunk.items():
-                if node_name == "chatbot_agent":
-                    chunk_answer = node_results.get("answer", "")
-                    yield chunk_answer
+        for chunk in graph.stream(initial_state, stream_mode="messages"):
+            message_chunk, metadata = chunk
+            node_name = metadata.get('langgraph_node', '')
+            node_trigger = metadata.get('langgraph_triggers', '')[0]
+            if node_name == "chatbot_agent" and node_trigger == "aggregation_agent":
+                chunk_answer = getattr(message_chunk, 'content', '')
+                yield chunk_answer
 
     return StreamingResponse(
         event_stream(user_request, location), media_type="text/event-stream"
