450a589331747d7659a5a30f2fa27ad8eee1b7b6
Marcel Tracz <marcel.tracz@wp.pl>
2024-12-09 23:53:27 +0100

    front

--- pages/Disease_Hotspots.py
+++ pages/Disease_Hotspots.py
@@ -37,24 +37,24 @@ LAT_CENTER = 20.7967
 LON_CENTER = -156.3319
 DEFAULT = pd.DataFrame({"LAT": [LAT_CENTER], "LON": [LON_CENTER]})
 
-df = pd.read_csv(DATASET, sep=";", header=None, names=["USER_ID", "DISEASE_ID", "DATE", "LAT", "LON"])
+df = pd.read_csv(DATASET, sep=";", header=None, names=["USER_ID", "DISEASE", "DATE", "LAT", "LON"])
 df["USER_ID"] = df["USER_ID"].astype("string")
-df["DISEASE_ID"] = df["DISEASE_ID"].astype("int")
+df["DISEASE"] = df["DISEASE"].astype("string")
 df["DATE"] = pd.to_datetime(df["DATE"], format="%d.%m.%Y", errors='coerce')
 df["LAT"] = df["LAT"].astype("float")
 df["LON"] = df["LON"].astype("float")
 
 min_date = df["DATE"].min().date()
 max_date = df["DATE"].max().date()
-unique_diseases = sorted(list(df["DISEASE_ID"].unique()))
-colors = plt.cm.get_cmap("viridis", len(unique_diseases))
+unique_diseases = list(df["DISEASE"].unique())
+colors = plt.get_cmap("viridis", min(len(unique_diseases), 256))
 colors = [rgba_to_hex(colors(i)) for i in range(len(unique_diseases))]
 
-df["COLOR"] = df["DISEASE_ID"].map(dict(zip(unique_diseases, colors)))
+df["COLOR"] = df["DISEASE"].map(dict(zip(unique_diseases, colors)))
 
 fig1, ax1 = plt.subplots()
 ax1.set_xlabel("Date")
-ax1.set_ylabel("New Infections")
+ax1.set_ylabel("New Cases")
 ax1.yaxis.set_major_locator(MaxNLocator(integer=True))
 
 plt.xticks(rotation=45, ha='right')
@@ -62,28 +62,31 @@ plt.tight_layout()
 
 fig2, ax2 = plt.subplots()
 ax2.set_xlabel("Date")
-ax2.set_ylabel("Cumulative Number of Infections")
+ax2.set_ylabel("Cumulative Number of Cases")
 ax2.yaxis.set_major_locator(MaxNLocator(integer=True))
 
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.grid()
 
+fig3, ax3 = plt.subplots()
+ax3.axis("equal")
+
 model = Lasso()
 
 with st.sidebar:
     start_date = st.date_input(label="Start date", min_value=min_date, max_value=max_date)
     end_date = st.date_input(label="End date", min_value=start_date, max_value=max_date)
-    main_df = df[(df["DATE"].dt.date >= start_date) & (df["DATE"].dt.date <= end_date)][["DISEASE_ID", "DATE", "LAT", "LON", "COLOR"]]
+    main_df = df[(df["DATE"].dt.date >= start_date) & (df["DATE"].dt.date <= end_date)][["DISEASE", "DATE", "LAT", "LON", "COLOR"]]
     
 if not main_df.empty:
     ids = ["All"] + unique_diseases
     disease_id = st.sidebar.selectbox("Choose disease ID", ids, key="disease_id")
-    st.write("### Reported Infection Cases:")
+    st.write("### Reported Cases:")
     count_sick = 0
 
     if disease_id == "All":
-        count_sick = main_df["DISEASE_ID"].count()
+        count_sick = main_df["DISEASE"].count()
         
         if count_sick == 0:
             st.map(DEFAULT, color="#00000000", zoom=8)
@@ -92,35 +95,42 @@ if not main_df.empty:
             st.map(main_df, color="COLOR", zoom=9)
 
             if start_date != end_date:
+                ratio = []
+                
                 for color_idx, id  in enumerate(unique_diseases):
-                    count_per_date = main_df[main_df['DISEASE_ID'] == id].groupby("DATE")["DISEASE_ID"].count()
+                    count_per_date = main_df[main_df['DISEASE'] == id].groupby("DATE")["DISEASE"].count()
                     cumulative_count = count_per_date.cumsum()
-                    ax2.plot(cumulative_count.index, cumulative_count, marker='o', linestyle='-', label="Disease " + str(id), color=colors[color_idx])
+                    ax2.plot(cumulative_count.index, cumulative_count, marker='o', linestyle='-', label=id, color=colors[color_idx])
+                    ratio.append(count_per_date.sum())
 
                 ax2.legend()
 
                 st.write("### Predicting the Disease Spread Rate:")
                 st.pyplot(fig2)
-    
+
+                ax3.pie(ratio, labels=unique_diseases, autopct='%1.1f%%', startangle=90, colors=colors)
+                st.write("### Disease Distribution Analysis:")
+                st.pyplot(fig3)
+
     else:
-        count_sick = count_disease_by_id(main_df, "DISEASE_ID", disease_id)
+        count_sick = count_disease_by_id(main_df, "DISEASE", disease_id)
 
         if count_sick == 0:
             st.map(DEFAULT, color="#00000000", zoom=8)
         
         else:
             color_idx = unique_diseases.index(disease_id)
-            st.map(main_df[main_df["DISEASE_ID"] == disease_id], color=colors[color_idx], zoom=9)        
+            st.map(main_df[main_df["DISEASE"] == disease_id], color=colors[color_idx], zoom=9)        
 
             if start_date != end_date:
-                count_per_date = main_df[main_df["DISEASE_ID"] == disease_id].groupby("DATE")["DISEASE_ID"].count()            
+                count_per_date = main_df[main_df["DISEASE"] == disease_id].groupby("DATE")["DISEASE"].count()            
                 cumulative_count = count_per_date.cumsum()
 
                 ax1.hist(count_per_date.index, bins=len(count_per_date), weights=count_per_date, color=colors[color_idx], edgecolor='black')
                 st.write("### Analysis of New Cases:")
                 st.pyplot(fig1)
                  
-                ax2.plot(cumulative_count.index, cumulative_count, color=colors[color_idx], marker='o', linestyle='-', label="Cumulative Number of Infections")
+                ax2.plot(cumulative_count.index, cumulative_count, color=colors[color_idx], marker='o', linestyle='-', label="Cumulative Number of Cases")
                 predict_data(cumulative_count)
 
                 st.write("### Predicting the Disease Spread Rate:")
@@ -136,10 +146,10 @@ if not main_df.empty:
 
     with col3:
         if disease_id != "All":
-            unique_users = main_df["DISEASE_ID"].count()
+            unique_users = main_df["DISEASE"].count()
             infected_percentage = (count_sick / unique_users) * 100 if unique_users > 0 else 0
             st.metric("Percentage Infected", value=f"{round(infected_percentage, 2)}%")
-        
+
     if disease_id != "All":
-        st.write("### Detailed Data for Disease", str(disease_id))
-        st.dataframe(main_df[main_df["DISEASE_ID"] == disease_id])
+        st.write("### Detailed Data for", disease_id)
+        st.dataframe(main_df[main_df["DISEASE"] == disease_id])
